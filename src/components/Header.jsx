e507118cd7c214f6c191749266b4d9eaec6fddda
Davor Grinfelder <grinfelderdavor@gmail.com>
2024-07-18 23:31:03 +0200

    sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,24 @@
 import { Link } from 'react-router-dom';
-import image from '../assets/image.jpg'
+import image from '../assets/image.jpg';
+import Sidebar from './Sidebar';
+
+//icons
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+import { useState } from 'react';
+
+
 
 function Header() {
+    const [isActive, setIsActive] = useState(false);
+
+    function toggleSidebar() {
+        setIsActive(!isActive);
+
+    }
     return (
-        <div className="container mx-auto flex-col items-center shadow-2xl md:flex md:justify-between md:flex-row md:bg-green-300 md:h-0 lg:w-10/12 lg:h-28">
+        <div className="container mx-auto flex-col items-center md:shadow-2xl md:flex md:justify-between md:flex-row md:bg-green-300 md:h-0 lg:w-10/12 lg:h-28">
             <div className=" flex flex-row justify-center items-center">
                 <img src={image} alt="aaa" className="w-28 h-28 " />
                 <h2 className="font-bold text-5xl ml-4 text-green-500">GreenGo</h2>
@@ -16,6 +31,22 @@ function Header() {
                     <Link to={'/about'} className=' hover:text-orange-300 focus:underline focus:decoration-orange-400 transition-all '>About Us</Link>
                     <Link to={'/contact'} className=' hover:text-orange-300 focus:underline focus:decoration-orange-400 transition-all '>Contact Us</Link>
                 </ul>
+            </div>
+            <div className='flex gap-5 mr-5 justify-center'>
+                <Link>
+                    <FaRegHeart size={28} color='green' />
+                </Link>
+
+                <Link>
+                    <FaRegUser size={28} color='green' />
+                </Link>
+
+                <Link>
+                    <MdOutlineShoppingCart size={28} color='green' onClick={toggleSidebar} />
+                </Link>
+            </div>
+            <div>
+                <Sidebar isActive={isActive} closedSidebar={toggleSidebar} />
             </div>
         </div>
     )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,22 @@
+//icons
+import { ImCross } from "react-icons/im";
 
-
-function Sidebar() {
+function Sidebar({ isActive, closedSidebar }) {
     return (
-        <div>Sidebar</div>
+        <div>
+            <div>
+                <div style={{ transform: `translateX(${isActive ? '0%' : '100%'})` }} className="fixed top-0 right-0 bg-white shadow-lg h-full transition-transform duration-300 ease-out">
+                    <div className="border-b m-4">
+                        <h1 className="text-3xl p-4">Your Cart</h1>
+                    </div>
+                    <div className="p-4">
+                        <span className="absolute top-0 right-0 p-4" onClick={closedSidebar}>
+                            <ImCross />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
