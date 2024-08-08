afe575cf0c0b4bfa35d0e70fa07ead6e216efe9a
Davor Grinfelder <grinfelderdavor@gmail.com>
2024-08-08 12:00:32 +0200

    redux middleware used fot the first time to save/get/load cart in/from ls

--- src/store/store.js
+++ src/store/store.js
@@ -4,14 +4,46 @@ import CartSlice from "../slices/CartSlice";
 import FavoriteSlice from "../slices/FavoriteSlice";
 
 
+//save to ls
+const saveCartState = (state) => {
+    try {
+        const pushToLs = JSON.stringify(state);
+        localStorage.setItem('cart', pushToLs);
+    } catch (err) {
+        console.error("Could not save state", err);
+    }
+};
 
+//get from ls
+const loadCartState = () => {
+    try {
+        const pushToLs = localStorage.getItem('cart');
+        if (pushToLs === null) {
+            return undefined;
+        }
+        return JSON.parse(pushToLs);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+//
+const preloadedState = {
+    cartStore: loadCartState()
+};
 
 const store = configureStore({
     reducer: {
         productStore: ProductSlice,
         cartStore: CartSlice,
         favoriteStore: FavoriteSlice,
-    }
-})
+    },
+    preloadedState
+});
+
+
+store.subscribe(() => {
+    saveCartState(store.getState().cartStore);
+});
 
 export default store;
