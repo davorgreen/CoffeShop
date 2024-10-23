4b776e505b556e238767aedfb1d2094450687db0
Davor Grinfelder <grinfelderdavor@gmail.com>
2024-10-23 15:33:54 +0200

    style updated

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -25,10 +25,10 @@ function CardComponent({ item, label, handleOpen, handleClose, modalOpen, handle
         <div className="gap-4 mt-10 p-5 md:flex-row justify-center items-center bg-green-200 shadow-2xl rounded-lg overflow-hidden m-4 relative">
             {/* Left side */}
             <div className="flex justify-center items-center">
-                <img src={image_url} alt={name} className="w-80 h-96 object-cover" />
+                <img src={image_url} alt={name} className="w-80 h-80 object-cover" />
             </div>
             {/* Right side */}
-            <div className="flex flex-col p-4 gap-2 justify-between text-center items-center h-full" style={{ height: '250px' }}>
+            <div className="flex flex-col p-4 gap-2 justify-between text-center items-center h-full" style={{ height: '280px' }}>
                 <div className=" flex flex-col gap-4">
                     <div className="text-xl text-green-800 font-bold underline">{name}</div>
                     <div className="text-gray-700 font-bold">{description}</div>
